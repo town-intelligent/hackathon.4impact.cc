c8b5bf96b70e87d77f167e16675ec3ddcd828573
HuangJyunYu <yillkid@gmail.com>
2023-09-07 14:20:44 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -3,15 +3,15 @@ HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
 HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
 
 // EID
-HOST_URL_EID = "https://eid.townway.com.tw";
+HOST_URL_EID = "https://alpha-passport.4impact.cc/";
 
 // Site hoster
-SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
+SITE_HOSTERS = ["ti@4impact.cc"]
 
 // Site information
-SITE_TYPE = 0 // 0 or 1(公板)
-SITE_NAME = "TPLANET"
-COPYRIGHT = "TPLANET"
+SITE_TYPE = 1 // 0 or 1(公板)
+SITE_NAME = "山城數位黑客松"
+COPYRIGHT = "南投縣政府"
 
 // Weight : SDGs, 人文地產景, 德智體群美
-WEIGHT = [1, 1, 1]
+WEIGHT = [1, 0, 0]
